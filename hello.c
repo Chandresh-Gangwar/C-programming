ad36ed56d0ce565b3c14a04e7db6b5b4eecfa466
Chandresh-Gangwar <72911021+Chandresh-Gangwar@users.noreply.github.com>
2021-01-22 11:27:38 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Hello world\n");
+    printf("enter any number=");
+    scanf("%d",&num);
+    printf("your number is %d",num);
+    return 0;
+}
